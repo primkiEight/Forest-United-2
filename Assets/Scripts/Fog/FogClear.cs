e0ad81105d5234b749f7fb7e1747133dc75312f3
Mario Primorac <primki@gmail.com>
2019-04-27 23:02:34 +0200

    Working on fog...

---
+++ Assets/Scripts/Fog/FogClear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogClear : MonoBehaviour {
+
+    public void ClearFog()
+    {
+        StartCoroutine(CoClearFog());        
+    }
+
+    private IEnumerator CoClearFog()
+    {
+        float timer = Random.Range(0.2f, 0.4f);
+
+        yield return new WaitForSeconds(timer);
+
+        Fog[] allChildren = GetComponentsInChildren<Fog>();
+
+        foreach (Fog child in allChildren)
+        {
+            child.AnimateFog();
+        }
+
+        Destroy(gameObject);
+    }
+}
